f03be3b5ceb0cfa7e736b7db922132558a338e9d
Zeke Thornberg <zekethornberg@gmail.com>
2026-02-01 18:46:36 -0600

    Actually fix the project: Remove all duplicate plugin directories and clean up workspace
    - Remove plugin/, minimal-plugin/, working-plugin/, kingdom-manager/, earth/, Kingdoms/ directories
    - Eliminate all duplicate plugin files and conflicting projects
    - Keep only the main plugins/KingdomCommands/ as the single source of truth
    - Clean workspace structure for proper deployment

--- minimal-plugin/KingdomPlugin.java
+++
@@ -1,8 +0,0 @@
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class KingdomPlugin extends JavaPlugin {
-    @Override
-    public void onEnable() {
-        getLogger().info("Kingdom Plugin Enabled!");
-    }
-}
